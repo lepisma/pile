06a12e97724d882fd8d39ab27913d7ffdc50385a
Abhinav Tushar <lepisma@fastmail.com>
2019-05-06 00:31:42 +0530

    tests for tag and option parsing

--- test/pile-test.el
+++ test/pile-test.el
@@ -1,5 +1,26 @@
+(require 'pile-tags)
 (require 'pile-utils)
+
+
+(defmacro with-string-in-buffer (string &rest body)
+  (declare (indent defun))
+  `(with-temp-buffer
+     (insert ,string)
+     (goto-char (point-min))
+     ,@body))
 
 (describe "Option parsing"
   (it "works"
-    (expect (pile--parse-option "draft:nil dropcap:t") :to-equal '((draft . nil) (dropcap . t)))))
+    (expect
+     (with-string-in-buffer
+       "#+PILE: draft:nil dropcap:t"
+       (pile-read-options))
+     :to-equal '((draft . nil) (dropcap . t)))))
+
+(describe "Tags parsing"
+  (it "works"
+    (expect
+     (with-string-in-buffer
+       "#+TAGS: hello-world, testing,this"
+       (pile-tags-parse-buffer))
+     :to-equal '("hello-world" "testing" "this"))))
